15171eb4fd95d34a738d63b2241b4886e1a8da38
Rafaela <rafaelaleal1818@gmail.com>
2025-01-25 12:34:17 +0000

    ddd

---
+++ funcaoprintinput.py
@@ -0,0 +1,18 @@
+import math
+
+#pedir ao utilizador um número inteiro e devolver a sua raíz quadrada
+
+numero=int(input("intruduza um número inteiro:"))
+#numero=int(numero)
+raizquadrada=math.sqrt(numero)
+
+print("A raíz quadrada de", numero,"é igual a", round(raizquadrada,2))
+
+
+#formatação posicional %d para string e %f para decimal
+print("A raíz quadrada do número:%d é igual a %.2f" %(numero,round(raizquadrada,2)))
+#usando format
+print("A raíz quadrada do número:{} é igual a {}.format(numero,round(raizquadrada,2))
+
+#mais atual e mais eficiente 
+print(f"A raíz quadrada do número:{numero} é igual a {round(raizquadrada,2)}")
