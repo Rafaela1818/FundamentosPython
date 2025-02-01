51942d7d2020a743a73cc3907bfc3f5e86657aaf
Rafaela1818 <rafaelaleal1818@gmail.com>
2025-02-01 12:18:42 +0000

    exercicio1ficha3

---
+++ FT03/ex01.py
@@ -0,0 +1,8 @@
+import math
+
+numero = int(input("infique um número inteiro: \t"))
+if numero > 20:
+    numero = numero/2
+    print("o resultado da divisão so seu número por 2 é: ", round(numero, 1))
+    
+else: print("O número indicado é inferior a 20")
