983f6ff4d4b923af1fca6a09fb65494ba8dcceb1
Rafaela1818 <rafaelaleal1818@gmail.com>
2025-02-02 15:15:38 +0000

    ficha2.4

---
+++ FT02/Ex04.py
@@ -0,0 +1,8 @@
+# Escreve um programa que solicite duas notas ([nota1] e [nota2]) ao utilizador e apresente a média das mesmas da seguinte forma:  
+
+Nota1= float(input("insira a primeira nota: "))
+Nota2= float(input("insira a segunda nota: "))
+
+media=(Nota1 + Nota2) /2
+
+print(f"A média das notas {Nota1} e {Nota2} é {media:10.2f}.")
