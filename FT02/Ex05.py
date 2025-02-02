7c6272c5594ded4840a8c5968ad6be96139fe6e7
Rafaela1818 <rafaelaleal1818@gmail.com>
2025-02-02 15:34:59 +0000

    ficha2.5.2

--- FT02/Ex05.py
+++ FT02/Ex05.py
@@ -1,6 +1,6 @@
 #Num ficheiro diferente dos utilizados nas alíneas anteriores, escreve um programa que faça a conversão para kms, de um dado valor em metros.
 
 Valorm= float(input("Insira o valor em metros:"))
-Valorkm= Valorm/1000
+Valorkm= Valorm /1000
 
 print(f"O valor em metros {Valorm} é igual a {Valorkm:.3f} km")
