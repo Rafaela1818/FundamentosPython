d549c5d6ef1a5e88e76a3fe6c4a4124baaeddf44
Rafaela1818 <rafaelaleal1818@gmail.com>
2025-02-02 17:11:37 +0000

    ficha3.2

---
+++ FT03.2/Ex02.py
@@ -0,0 +1,11 @@
+#Implementa um programa que leia dois números e indique se estes são iguais ou diferentes.
+
+import math
+
+mun1= float(input("Digite o primeiro número:"))
+num2= float(input("Digite o segundo número:"))
+
+if num1 == num2:
+    print("Os números são iguais")
+else:
+    print("Os números são diferentes")
