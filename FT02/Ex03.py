deae1683c673b0c642eebeb7e4ad886f6af7dacb
Rafaela1818 <rafaelaleal1818@gmail.com>
2025-02-02 15:00:32 +0000

    ficha2.4

---
+++ FT02/Ex03.py
@@ -0,0 +1,11 @@
+#Escreve um programa em python que solicite três números inteiros ao utilizador e imprima a soma dos mesmos.
+
+
+
+num1 = int(input("Insira o primeiro número:"))
+num2 = int(input("Insira o segundo número:"))
+num3 = int(input("insira o terceiro número:"))
+
+soma= num1 + num2 + num3 
+print("A soma dos números é:", soma)
+
