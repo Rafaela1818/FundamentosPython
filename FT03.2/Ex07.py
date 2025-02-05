804f2d11104ed99ab1d56eb44b10f3ba6e056457
Rafaela1818 <rafaelaleal1818@gmail.com>
2025-02-05 21:31:54 +0000

    ficha3.6.2

--- FT03.2/Ex07.py
+++ FT03.2/Ex07.py
@@ -1,17 +1,9 @@
 #Elabora um programa para verificar se um ano é bissexto ou não. A condição para ser 
 #um ano bissexto é: o ano deve ser divisível por 400; ou se for divisível por 4 e não for divisível por 100.
 
-def is_bissexto(ano):
-    if (ano % 400 == 0) or (ano % 4 == 0 and ano % 100 != 0):
-        return True
-    else:
-        return False
+ano_bissexto=int(input("Intruduza o ano:"))
 
-# Solicita ao usuário para inserir um ano
-ano = int(input("Digite um ano: "))
-
-# Verifica se o ano é bissexto e exibe o resultado
-if is_bissexto(ano):
-    print(f"{ano} é um ano bissexto.")
+if ano_bissexto % 400 == 0 or (ano_bissexto %4 == 0 and ano_bissexto %100 != 0):
+     print("O ano é bissexto")
 else:
-    print(f"{ano} não é um ano bissexto.")
+     print("O ano não é bissexto")
