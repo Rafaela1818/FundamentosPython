88fa4b214af43315743cdb6abc3160adfebbd28a
Rafaela1818 <rafaelaleal1818@gmail.com>
2025-02-07 22:54:40 +0000

    ficha3.10.1

--- Ex10.py
+++ Ex10.py
@@ -3,3 +3,23 @@
 #mensagem personalizada. Para os restantes casos, mostra no ecrã o resultado da 
 #operação desejada.
 
+
+calculadora =input("escolha uma operação +,-,*,/:")
+valor1= float(input("Intrduza o primeiro valor:"))
+valor2= float(input("Introduza o segundo valor:"))
+
+if calculadora == "+":
+    resultado = valor1 + valor2
+    print(f"O resultado de {valor1} + {valor2} é: {resultado}")
+elif calculadora == "-":
+    resultado = valor1 - valor2
+    print(f"O resultado de {valor1} - {valor2} é: {resultado}")
+elif calculadora == "*":
+    resultado = valor1 * valor2
+    print(f"O resultado de {valor1} * {valor2} é: {resultado}")
+elif calculadora == "/":
+    if  valor2 == 0:
+        print("Não é possível dividir por zero.")
+    else:
+        resultado = valor1 / valor2
+        print(f"O resultado de {valor1} / {valor2} é: {resultado}")
