ef8510ccc7dada470b92aaebfb6cd03365f2637a
Rafaela1818 <rafaelaleal1818@gmail.com>
2025-02-08 12:18:33 +0000

    fichapw2.2

---
+++ ExecercioPW.2/Ex01.py
@@ -0,0 +1,11 @@
+# Escreve um programa que calcule a soma dos 10 primeiros números inteiros positivos e devolve o resultado para o ecã
+
+i =1
+soma= 0
+while i <= 10:
+    soma =soma + i
+    print(soma)
+    i += 1
+    
+print("A soma dos 10 primeiros números inteiros positivos é:", soma)
+        
