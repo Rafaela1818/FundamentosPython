e5da78db426204236d1d13aa974d5eeaa8591def
Rafaela1818 <rafaelaleal1818@gmail.com>
2025-02-08 10:12:17 +0000

    ficha3

--- FT03.2/Ex08.py
+++ FT03.2/Ex08.py
@@ -1,9 +1,9 @@
 #Escreve um programa para classificar um triângulo de acordo com o comprimento dos seus lados. 
 
 print("Introduza o cumprimento dos lados do triângulo:")
-l1=input("Lado 1:")
-l2=input("Lado 2:")
-l3=input("Lado 3:")
+l1=float(input("Lado 1:"))
+l2=float(input("Lado 2:"))
+l3=float(input("Lado 3:"))
 
 if l1 == l2 == l3:
     print("O triangulo é Equilátero")
---
+++ FT04/Ex01.py
@@ -0,0 +1,4 @@
+#Faz um programa que escreva o nome do mês que é introduzido, pelo utilizador, na forma numérica.
+
+Nome_mes= input("Introduza o nome do mês")
+
