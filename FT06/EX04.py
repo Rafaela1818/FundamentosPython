a4fbcad4958b61c3b0f2e11463935cb6ac94edf7
Rafaela1818 <rafaelaleal1818@gmail.com>
2025-03-01 12:43:07 +0000

    ficha6.3.4

--- FT06/EX04.py
+++ FT06/EX04.py
@@ -5,12 +5,44 @@ a. Imprima a quantidade de inteiros, floats, strings e boleanos na lista;
 b. Efetua a média de todos os valores inteiros na lista. 
 c. Crie e retorne uma nova lista só com os valores inteiros"""
 
-nums=[10, 2.5, 7, 11, 7.9, "Python", True,6, 5.8 , "Listas"] 
+# Lista fornecida
+nums = [10, 2.5, 7, 11, 7.9, "Python", True, 6, 5.8, "Listas"]
 
-#a. Imprima a quantidade de inteiros, floats, strings e boleanos na lista
+# a. Imprime a quantidade de inteiros, floats, strings e booleanos na lista
+contagem = {
+    'inteiros': 0,
+    'floats': 0,
+    'strings': 0,
+    'booleanos': 0
+}
 
-inteiros = [x for x in nums if from django.conf import settings]
-floats = [x for x in nums if from django.conf import settings ]
+# Lista para armazenar os inteiros
+lista_inteiros = []
 
-#b. Efetua a média de todos os valores inteiros na lista
+# Itera sobre a lista para contar os tipos e coletar inteiros
+for item in nums:
+    if isinstance(item, int):
+        contagem['inteiros'] += 1
+        lista_inteiros.append(item)  
+    elif isinstance(item, float):
+        contagem['floats'] += 1
+    elif isinstance(item, str):
+        contagem['strings'] += 1
+    elif isinstance(item, bool):
+        contagem['booleanos'] += 1
 
+# Imprime a contagem
+print("Quantidade de inteiros:", contagem['inteiros'])
+print("Quantidade de floats:", contagem['floats'])
+print("Quantidade de strings:", contagem['strings'])
+print("Quantidade de booleanos:", contagem['booleanos'])
+
+# b. Efetua a média de todos os valores inteiros na lista
+if contagem['inteiros'] > 0:
+    media_inteiros = sum(lista_inteiros) / contagem['inteiros']
+    print("Média dos valores inteiros:", media_inteiros)
+else:
+    print("Não há valores inteiros para calcular a média.")
+
+# c. Cria e retorna uma nova lista só com os valores inteiros
+print("Nova lista com valores inteiros:", lista_inteiros)
---
+++ FT06/Ex04_v2.py
@@ -0,0 +1,33 @@
+""" Considera a seguinte lista: 
+nums=[10, 2.5, 7, 11, 7.9, "Python", True,6, 5.8 , "Listas"] 
+Efetua um programa em python que: 
+a. Imprima a quantidade de inteiros, floats, strings e boleanos na lista; 
+b. Efetua a média de todos os valores inteiros na lista. 
+c. Crie e retorne uma nova lista só com os valores inteiros"""
+
+nums = [10, 2.5, 7, 11, 7.9, 'Python', True, 6, 5.8, 'Listas']
+# a. Imprima a quantidade de inteiros, floats, strings e boleanos na lista;
+inteiros = 0
+floats = 0
+strings = 0
+boleanos = 0
+# Usando for loop & type
+for num in nums:
+    if type(num) == int:
+        inteiros += 1
+    elif type(num) == float:
+        floats += 1
+    elif type(num) == str:
+        strings += 1
+    elif type(num) == bool:
+        boleanos += 1
+# (EXTRA) Usando list comprehension
+inteiros = len([ num for num in nums if type(num) == int   ])
+floats   = len([ num for num in nums if type(num) == float ])
+strings  = len([ num for num in nums if type(num) == str   ])
+boleanos = len([ num for num in nums if type(num) == bool  ])
+
+print(f'Quantidade de inteiros: {inteiros}')
+print(f'Quantidade de floats: {floats}')
+print(f'Quantidade de strings: {strings}')
+print(f'Quantidade de boleanos: {boleanos}')
